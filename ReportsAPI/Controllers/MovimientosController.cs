d89d86a920ef52f951d301994dd6fc52b72ab6ce
Christian Johanny Silva <christiamsilva@hotmail.es>
2023-06-22 22:18:41 -0600

    Avance del reporte api que hace falta.

--- ReportsAPI/Controllers/MovimientosController.cs
+++ ReportsAPI/Controllers/MovimientosController.cs
@@ -24,27 +24,27 @@ namespace ReportsAPI.Controllers
 
         public IActionResult AjusteSalida(int id)
         {
-            var array = (from a in _db.OutPutProducts 
+            var array = (from a in _db.OutPutProducts
                          join b in _db.OutPutProductDetails on a.Id equals b.OutPutProductId
                          join c in _db.Areas on a.AreaId equals c.Id
                          join d in _db.Products on b.ProductId equals d.Id
                          join e in _db.Users on a.CreateBy equals e.Username
                          where a.Id == id
                          select new MovimientoBase()
-            {
-                Id = string.Format("#{0}", a.Id),
-                Date = a.Date,
-                Area = c.Name,
-                Username = e.FullName,
-                Tipo = "Ajuste de Salida",
-                Observation = a.Observation,
-                ProductName = d.Name,
-                ProductCost = b.Cost,
-                ProductQuantity = b.Quantity,
-                ProductTotal = b.Total,
-                ProductId = b.ProductId,
-                Total = a.Total
-            });
+                         {
+                             Id = string.Format("#{0}", a.Id),
+                             Date = a.Date,
+                             Area = c.Name,
+                             Username = e.FullName,
+                             Tipo = "Ajuste de Salida",
+                             Observation = a.Observation,
+                             ProductName = d.Name,
+                             ProductCost = b.Cost,
+                             ProductQuantity = b.Quantity,
+                             ProductTotal = b.Total,
+                             ProductId = b.ProductId,
+                             Total = a.Total
+                         });
             xrSaldoInicial _xrSaldoInicial = new xrSaldoInicial()
             {
                 DataSource = array
@@ -102,83 +102,93 @@ namespace ReportsAPI.Controllers
         //    return (new PdfServices()).ExportPdf(_xrSaldoInicial);
         //}
 
-        //private MovimientoRequisaDespacho[] QueryRequisa(int id, string tipo)
-        //{
-        //    var array = _db.Traslates
-        //        .Include(a => a.TraslateDetails)
-                
+        private MovimientoRequisaDespacho[] QueryRequisa(int id, string tipo)
+        {
+            var array = _db.Traslates
+                .Where(cond => cond.Id == id)
+                .Include(b => b.AreaSource)
+                .Include(c => c.AreaTarget)
+                .Include(d => d.Stage)
+                .Include(a => a.TraslateDetails)
+                .Include(f => f.TraslateDetails.)
+                .ThenInclude(e => e.Product)
+                .Select(s => new MovimientoRequisaDespacho()
+                {
+                    Id = string.Format("#{0}", s,id),
+                    Date = s.Date,
+                    Estado = .Name,
+                    AreaSource = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.aSource.Name,
+                    AreaTarget = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.aTarget.Name,
+                    Username = u003cu003eh__TransparentIdentifier5.u.FullName,
+                    Tipo = tipo,
+                    Observation = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Observation,
+                    ProductName = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.pro.Name,
+                    ProductCost = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.Cost,
+                    ProductQuantity = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.QuantityRequest,
+                    ProductResponse = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.QuantityResponse,
+                    ProductTotal = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.Total,
+                    ProductId = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.ProductId,
+                    Total = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Total,
+                    ModifyBy = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.LastModificationBy
+                })
 
-        //    MovimientoRequisaDespacho[] array = Enumerable.ToArray<MovimientoRequisaDespacho>(
-        //        Queryable.Select(Queryable.Where(Queryable.Join(
-        //            Queryable.Join(Queryable.Join(Queryable.Join(
-        //                Queryable.Join(Queryable.Join(this._db.Traslates, this._db.TraslateDetails, 
-        //                (Traslates t) => t.Id, (TraslateDetails td) => td.TraslateId, 
-        //                (Traslates t, TraslateDetails td) => new { t = t, td = td }), 
-        //                this._db.TraslateStages, (u003cu003eh__TransparentIdentifier0) => 
-        //                u003cu003eh__TransparentIdentifier0.t.StageId, (TraslateStages e) => e.Id, 
-        //                (u003cu003eh__TransparentIdentifier0, e) => new { <> h__TransparentIdentifier0 = u003cu003eh__TransparentIdentifier0, e = e }), 
-        //                this._db.Areas, (u003cu003eh__TransparentIdentifier1) => 
-        //                u003cu003eh__TransparentIdentifier1.<> h__TransparentIdentifier0.t.AreaSourceId, 
-        //                (Areas aSource) => aSource.Id, (u003cu003eh__TransparentIdentifier1, aSource) => 
-        //                new { <> h__TransparentIdentifier1 = u003cu003eh__TransparentIdentifier1, aSource = aSource }), 
-        //                this._db.Areas, (u003cu003eh__TransparentIdentifier2) => 
-        //                u003cu003eh__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.AreaTargetId, 
-        //                (Areas aTarget) => aTarget.Id, (u003cu003eh__TransparentIdentifier2, aTarget) => 
-        //                new { <> h__TransparentIdentifier2 = u003cu003eh__TransparentIdentifier2, aTarget = aTarget }), 
-        //                this._db.Products, (u003cu003eh__TransparentIdentifier3) => u003cu003eh__TransparentIdentifier3.<> 
-        //                h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.ProductId, 
-        //                (Products pro) => pro.Id, (u003cu003eh__TransparentIdentifier3, pro) => new 
-        //                { <> h__TransparentIdentifier3 = u003cu003eh__TransparentIdentifier3, pro = pro }), 
-        //            this._db.Users, (u003cu003eh__TransparentIdentifier4) => u003cu003eh__TransparentIdentifier4.<> 
-        //            h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.CreateBy, 
-        //            (Users u) => u.Username, (u003cu003eh__TransparentIdentifier4, u) => new { <> h__TransparentIdentifier4 = u003cu003eh__TransparentIdentifier4, u = u }), 
-        //            (u003cu003eh__TransparentIdentifier5) => u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> 
-        //            h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Id == id), 
-        //            (u003cu003eh__TransparentIdentifier5) => new MovimientoRequisaDespacho()
-        //    {
-        //        Id = string.Format("#{0}", (object)u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Id),
-        //        Date = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Date,
-        //        Estado = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.e.Name,
-        //        AreaSource = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.aSource.Name,
-        //        AreaTarget = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.aTarget.Name,
-        //        Username = u003cu003eh__TransparentIdentifier5.u.FullName,
-        //        Tipo = tipo,
-        //        Observation = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Observation,
-        //        ProductName = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.pro.Name,
-        //        ProductCost = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.Cost,
-        //        ProductQuantity = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.QuantityRequest,
-        //        ProductResponse = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.QuantityResponse,
-        //        ProductTotal = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.Total,
-        //        ProductId = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.ProductId,
-        //        Total = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Total,
-        //        ModifyBy = u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<> h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.LastModificationBy
-        //    }));
-        //    return array;
-        //}
 
-        //public IActionResult RequisaDespacho(int id)
-        //{
-        //    MovimientoRequisaDespacho[] movimientoRequisaDespachoArray = this.QueryRequisa(id, "Despacho de Requisa");
-        //    xrRequisaResponse _xrRequisaResponse = new xrRequisaResponse()
-        //    {
-        //        DataSource = movimientoRequisaDespachoArray
-        //    };
-        //    return (new PdfServices()).ExportPdf(_xrRequisaResponse);
-        //}
+        MovimientoRequisaDespacho[] array = Enumerable.ToArray<MovimientoRequisaDespacho>(
+            Queryable.Select(Queryable.Where(Queryable.Join(
+                Queryable.Join(Queryable.Join(Queryable.Join(
+                    Queryable.Join(Queryable.Join(this._db.Traslates, this._db.TraslateDetails,
+                    (Traslates t) => t.Id, (TraslateDetails td) => td.TraslateId,
+                    (Traslates t, TraslateDetails td) => new { t = t, td = td }),
+                    this._db.TraslateStages, (u003cu003eh__TransparentIdentifier0) =>
+                    u003cu003eh__TransparentIdentifier0.t.StageId, (TraslateStages e) => e.Id,
+                    (u003cu003eh__TransparentIdentifier0, e) => new { <> h__TrarentIdenansptifier0 = u003cu003eh__TransparentIdentifier0, e = e }),
+                    this._db.Areas, (u003cu003eh__TransparentIdentifier1) =>
+                    u003cu003eh__TransparentIdentifier1.<> h__TransparentIdentifier0.t.AreaSourceId,
+                    (Areas aSource) => aSource.Id, (u003cu003eh__TransparentIdentifier1, aSource) =>
+                    new { <> h__TransparentIdentifier1 = u003cu003eh__TransparentIdentifier1, aSource = aSource }),
+                    this._db.Areas, (u003cu003eh__TransparentIdentifier2) =>
+                    u003cu003eh__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.AreaTargetId,
+                    (Areas aTarget) => aTarget.Id, (u003cu003eh__TransparentIdentifier2, aTarget) =>
+                    new { <> h__TransparentIdentifier2 = u003cu003eh__TransparentIdentifier2, aTarget = aTarget }),
+                    this._db.Products, (u003cu003eh__TransparentIdentifier3) => u003cu003eh__TransparentIdentifier3.<>
+                    h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.td.ProductId,
+                    (Products pro) => pro.Id, (u003cu003eh__TransparentIdentifier3, pro) => new
+                    { <> h__TransparentIdentifier3 = u003cu003eh__TransparentIdentifier3, pro = pro }),
+                this._db.Users, (u003cu003eh__TransparentIdentifier4) => u003cu003eh__TransparentIdentifier4.<>
+                h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.CreateBy,
+                (Users u) => u.Username, (u003cu003eh__TransparentIdentifier4, u) => new { <> h__TransparentIdentifier4 = u003cu003eh__TransparentIdentifier4, u = u }),
+                (u003cu003eh__TransparentIdentifier5) => u003cu003eh__TransparentIdentifier5.<> h__TransparentIdentifier4.<>
+                h__TransparentIdentifier3.<> h__TransparentIdentifier2.<> h__TransparentIdentifier1.<> h__TransparentIdentifier0.t.Id == id),
+                (u003cu003eh__TransparentIdentifier5) => new MovimientoRequisaDespacho()
+                {
+                    
+                }));
+            return array;
+        }
 
-        //public IActionResult RequisaSolicitud(int id)
-        //{
-        //    MovimientoRequisaDespacho[] movimientoRequisaDespachoArray = this.QueryRequisa(id, "Solicitud de requisa");
-        //    RequisaSolicitud requisaSolicitud = new RequisaSolicitud()
-        //    {
-        //        DataSource = movimientoRequisaDespachoArray
-        //    };
-        //    return (new PdfServices()).ExportPdf(requisaSolicitud);
-        //}
+        public IActionResult RequisaDespacho(int id)
+        {
+            MovimientoRequisaDespacho[] movimientoRequisaDespachoArray = this.QueryRequisa(id, "Despacho de Requisa");
+            xrRequisaResponse _xrRequisaResponse = new xrRequisaResponse()
+            {
+                DataSource = movimientoRequisaDespachoArray
+            };
+            return (new PdfServices()).ExportPdf(_xrRequisaResponse);
+        }
 
-        //public IActionResult SaldoInicial(int id)
-        //{
-        //    return this.Movimientos(id, "Saldo Inicial");
-        //}
+        public IActionResult RequisaSolicitud(int id)
+        {
+            MovimientoRequisaDespacho[] movimientoRequisaDespachoArray = this.QueryRequisa(id, "Solicitud de requisa");
+            RequisaSolicitud requisaSolicitud = new RequisaSolicitud()
+            {
+                DataSource = movimientoRequisaDespachoArray
+            };
+            return (new PdfServices()).ExportPdf(requisaSolicitud);
+        }
+
+        public IActionResult SaldoInicial(int id)
+        {
+            return this.Movimientos(id, "Saldo Inicial");
+        }
     }
 }
